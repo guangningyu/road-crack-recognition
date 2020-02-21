896ca928aca70e996a36e326e70e108dd8ca43df
Guangning Yu <hi@guangningyu.com>
2020-02-21 14:33:51 +0000

    add script to generate thumbnails

---
+++ scripts/gen_thumbnail.py
@@ -0,0 +1,14 @@
+import os, sys
+from PIL import Image
+
+size = (960, 720)
+
+for infile in sys.argv[1:]:
+    outfile = os.path.splitext(infile)[0] + "_thumbnail.JPG"
+    if infile != outfile:
+        try:
+            im = Image.open(infile)
+            im.thumbnail(size)
+            im.save(outfile, "JPEG")
+        except IOError:
+            print("cannot create thumbnail for", infile)
